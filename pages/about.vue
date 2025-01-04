a5b5d6e3d4c13ba905734e99fe674cfe8c9cc191
nagi0705 <bellreuben1126@gmail.com>
2025-01-04 23:20:25 +0900

    7

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore voluptatem dicta, obcaecati possimus illum vel adipisci harum earum iure.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore voluptatem dicta, obcaecati possimus illum vel adipisci harum earum iure.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <p>Product details for {{ id }}</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quas aliquam similique atque sed tempora, cumque eius nihil enim nequam?</p>
+    </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+
+  definePageMeta({
+    layout: 'products'
+  })
+</script>
+
+<style scoped>
+
+</style>
